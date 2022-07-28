1af23d53ee67d4f9f71eb1ba4dadae675e082c6b
yorriyurri <skierorama@gmail.com>
2022-07-28 16:28:27 +0300

    edit infra folder

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -91,8 +91,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
+STATIC_URL = '/backend_static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'backend_static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
