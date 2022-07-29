091d7ac82bd7de5ccbe933f89d5d902a6fe073e7
Юрий Ивайкин <skierorama@gmail.com>
2022-07-29 23:43:23 +0300

    flake8

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -1,8 +1,6 @@
+from api.urls import urlpatterns
 from django.contrib import admin
 from django.urls import include, path
-
-from api.urls import urlpatterns
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
